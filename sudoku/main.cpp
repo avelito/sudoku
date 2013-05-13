86afc2efd35b877ed24a70c756869511d49cd5cf
avelito  <avelito@server.local>
2013-05-13 11:38:59 +0300

    Initial Commit

---
+++ sudoku/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sudoku
+//
+//  Created by avelito  on 5/13/13.
+//  Copyright (c) 2013 avelito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
